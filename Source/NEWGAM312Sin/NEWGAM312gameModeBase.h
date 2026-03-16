d2bc4882ec52ecc93d55e099213c2b3429d3f601
Sky Sin <da.sin@snhu.edu>
2026-03-16 00:39:23 -0400

    resources added

---
+++ Source/NEWGAM312Sin/NEWGAM312gameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NEWGAM312gameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEWGAM312SIN_API ANEWGAM312gameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/NEWGAM312Sin/PlayerChar.h
@@ -0,0 +1,92 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Camera/CameraComponent.h"
+#include "Resource_M.h"
+#include "Kismet/GameplayStatics.h"
+#include "PlayerChar.generated.h"
+
+UCLASS()
+class NEWGAM312SIN_API APlayerChar : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlayerChar();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION()
+		void MoveForward(float axisValue);
+
+	UFUNCTION()
+		void MoveRight(float axisValue);
+
+	UFUNCTION()
+		void StartJump();
+
+	UFUNCTION()
+		void StopJump();
+
+	UFUNCTION()
+		void FindObject();
+
+	UPROPERTY(VisibleAnywhere)
+		UCameraComponent* PlayerCamComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Status")
+		float Health = 100.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Status")
+		float Hunger = 100.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Status")
+		float Stamina = 100.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Resources")
+		int Wood;
+
+	UPROPERTY(EditAnywhere, Category = "Resources")
+		int Stone;
+
+	UPROPERTY(EditAnywhere, Category = "Resources")
+		int Berry;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resources")
+		TArray<int> ResourcesArray;
+
+	UPROPERTY(EditAnywhere, Category = "Resources")
+		TArray<FString> ResourcesNameArray;
+
+	UPROPERTY(EditAnywhere, Category = "HitMarker")
+		UMaterialInterface* hitDecal;
+
+	UFUNCTION(BlueprintCallable)
+		void SetHealth(float amount);
+
+	UFUNCTION(BlueprintCallable)
+		void SetHunger(float amount);
+
+	UFUNCTION(BlueprintCallable)
+		void SetStamina(float amount);
+
+	UFUNCTION()
+		void DecreaseStatus();
+
+	UFUNCTION()
+		void GiveResource(float amount, FString resourceType);
+
+};
